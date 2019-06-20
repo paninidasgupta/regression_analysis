46e8debd8bfd80fa6ce213828d9a56090683834d
paninidasgupta <panini.dasgupta@gmail.com>
2019-06-20 13:06:34 +0530

    1st commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='regression_analysis',
+    version='1.0.0',
+    url='https://github.com/paninidasgupta/regression_analysis.git',
+    author='Panini Dasgupta',
+    author_email='panini.dasgupta@gmail.com',
+    description='A simple regression analysis (climate variables)',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 3.0.3','scipy >= 1.2.1','xarray >= 0.12.0','basemap >= 1.2.0'],
+)
